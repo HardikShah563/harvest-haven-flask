645862a9f9edacd87894ffeb7c643dfb9d50e3a4
HardikShah563 <hardikts@gmail.com>
2023-08-01 14:56:56 +0530

    fixing nav links

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route('/')
-def index(): 
+def home(): 
     return render_template('home.html', session = session)
 
 @app.route('/signin')
