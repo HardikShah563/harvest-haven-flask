d6dbd3ee96fa260c8110aeb3222017ebf587f8df
HardikShah563 <hardikts@gmail.com>
2023-08-02 12:39:23 +0530

    sign in works

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 import psycopg2
 import psycopg2.extras
-# from database import *
+from database import *
 import hashlib
 
 app = Flask(__name__)
@@ -14,59 +14,113 @@ Session(app)
 
 @app.route('/')
 def home(): 
+    # print(hashlib.sha256("Mayuri@563".encode('utf-8')).hexdigest())
     return render_template('home.html', session = session)
 
-@app.route('/signin')
+@app.route('/signin', methods=["POST", "GET"])
 def signin(): 
-    return render_template('signin.html', session = session)
+    msgColor = ""
+    msgText = ""
+    if request.method == 'POST': 
+        email = request.form['email']
+        # passcode = hash(request.form['passcode'])
+        passcode = hashlib.sha256(request.form['passcode'].encode('utf-8')).hexdigest()
+        msg = loginAccount(email, passcode)
+        if(msg): 
+            msgColor = "green"
+            msgText = "Login Successful!" 
+            
+            session['u_id'] = getUID(email)
+            session['username'] = getName(email)
+            session['email'] = email
+            session['isAdmin'] = adminCheck(email)
+        else: 
+            msgColor = "red"
+            msgText = "Couldn't log you in, try again!"
+
+    return render_template('signin.html', msgColor = msgColor,  msg = msgText, session = session)
+
+# -------------------------------------------------------
 
 @app.route('/signup')
 def signup(): 
     return render_template('signup.html', session = session)
 
+# -------------------------------------------------------
+
 @app.route('/store')
 def store(): 
     return render_template('store.html', session = session)
+
+# -------------------------------------------------------
 
 @app.route('/cart')
 def cart(): 
     return render_template('cart.html', session = session)
 
+# -------------------------------------------------------
+
 @app.route('/checkout')
 def checkout(): 
     return render_template('checkout.html', session = session)
+
+# -------------------------------------------------------
 
 @app.route('/admin-dashboard')
 def adminDashboard(): 
     return render_template('adminDashboard.html', session = session)
 
+# -------------------------------------------------------
+
 @app.route('/admin-stats')
 def adminStats(): 
     return render_template('adminStats.html', session = session)
+
+# -------------------------------------------------------
 
 @app.route('/add-item')
 def addItem(): 
     return render_template('addItem.html', session = session)
 
+# -------------------------------------------------------
+
 @app.route('/edit-item')
 def editItem(): 
     return render_template('editItem.html', session = session)
+
+# -------------------------------------------------------
 
 @app.route('/delete-item')
 def deleteItem(): 
     return render_template('deleteItem.html', session = session)
 
+# -------------------------------------------------------
+
 @app.route('/add-category')
 def addCategory(): 
     return render_template('addCategory.html', session = session)
+
+# -------------------------------------------------------
 
 @app.route('/edit-category')
 def editCategory(): 
     return render_template('editCategory.html', session = session)
 
+# -------------------------------------------------------
+
 @app.route('/delete-category')
 def deleteCategory(): 
     return render_template('deleteCategory.html', session = session)
+
+# -------------------------------------------------------
+
+@app.route('/signout')
+def signout(): 
+    session["u_id"] = None
+    session["name"] = None
+    session["email"] = None
+    session["isAdmin"] = None
+    return redirect("/")
 
 
 if __name__ == '__main__': 
---
+++ database.py
@@ -0,0 +1,203 @@
+import psycopg2
+import psycopg2.extras
+
+hostname = 'localhost'
+database = 'harvesthaven'
+username = 'postgres'
+pwd = 'Hardikts@563'
+port_id = 5432
+
+conn = None
+cur = None
+
+conn = psycopg2.connect(
+    host = hostname,
+    dbname = database,
+    user = username,
+    password = pwd,
+    port = port_id
+)
+
+cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+
+# create_script = '''
+#     create table if not exists users (
+#         u_id integer not null, 
+#         username varchar(100) not null, 
+#         gender boolean not null, 
+#         email varchar(100) not null, 
+#         passcode varchar(200), 
+#         isAdmin boolean not null default false, 
+#         PRIMARY KEY (u_id)
+#     );
+
+#     create table if not exists category (
+#         c_id integer not null, 
+#         c_name varchar(100) not null, 
+#         PRIMARY KEY (c_id)
+#     );
+
+#     create table if not exists shop_items (
+#         s_id integer not null, 
+#         s_name varchar(100) not null, 
+#         s_pack_qty varchar(50), 
+#         s_price float not null, 
+#         s_stock_qty integer not null, 
+#         c_id integer not null, 
+#         FORIEGN KEY (c_id) REFERENCES category (c_id)
+#     );
+
+#     create table if not exists orders (
+#         o_id integer not null, 
+#         u_id integer not null, 
+#         name varchar(100) not null, 
+#         email varchar(100) not null, 
+#         addr varchar(200) not null, 
+#         city varchar(50) not null, 
+#         state varchar(50) not null, 
+#         zip integer not null, 
+#         order_total float not null, 
+#         purchase varchar(200) not null, 
+#         PRIMARY KEY (o_id), 
+#         FORIEGN KEY (u_id) REFERENCES users (u_id)
+#     );
+
+#     create sequence if not exists public.user_seq_no
+#         increment 1
+#         start 1
+#         minvalue 1
+#         maxvalue 99999
+#         owned by users.u_id;
+
+#     alter sequence public.user_seq_no
+#         owner to postgres;
+
+#     create sequence if not exists public.category_seq_no
+#         increment 1
+#         start 1
+#         minvalue 1
+#         maxvalue 99999
+#         owned by category.c_id;
+
+#     alter sequence public.category_seq_no
+#         owner to postgres;
+
+#     create sequence if not exists public.order_seq_no
+#         increment 1
+#         start 1
+#         minvalue 1
+#         maxvalue 99999
+#         owned by booking.booking_id;
+
+#     alter sequence public.order_seq_no
+#         owner to postgres;
+# '''
+# # gender - true is male and false is female
+# # card details wont be save therefore did not make columns for that
+# cur.execute(create_script)
+# conn.commit()
+
+# -------------------------------------------------------
+
+def registerAccount(name, gender, email, password): 
+    insert_script = '''
+        select u.email from users as u where email = %s
+    '''
+    insert_values = ([email])
+    cur.execute(insert_script, insert_values)
+    conn.commit()
+    if(cur.fetchall()):
+        return False
+
+    insert_script = '''
+        insert into users (u_id, username, gender, email, passcode) 
+        values (NEXTVAL('user_seq_no'), %s, %s, %s)
+    '''
+    insert_values = (name, gender, email, password)
+    cur.execute(insert_script, insert_values)
+    if(conn.commit()): 
+       return True
+
+# -------------------------------------------------------
+
+def loginAccount(email, password): 
+    insert_script = '''
+        select passcode from users where email = %s
+    '''
+    insert_values = ([email])
+    cur.execute(insert_script, insert_values)
+    conn.commit()
+    data = cur.fetchall()
+    if(not data):
+        return False
+    if(data[0][0] == password): 
+        return True
+    else: 
+        return False
+
+# -------------------------------------------------------
+
+def getUID(email): 
+    insert_script = '''
+        select u_id from users where email = %s
+    '''
+    insert_values = ([email])
+    cur.execute(insert_script, insert_values)
+    conn.commit()
+    data = cur.fetchone()
+    return data[0]
+
+# -------------------------------------------------------
+
+def getName(email): 
+    insert_script = '''
+        select username from users where email = %s
+    '''
+    insert_values = ([email])
+    cur.execute(insert_script, insert_values)
+    conn.commit()
+    data = cur.fetchone()
+    return data[0]
+
+# -------------------------------------------------------
+
+def adminCheck(email): 
+    insert_script = '''
+        select isadmin from users where email = %s
+    '''
+    insert_values = ([email])
+    cur.execute(insert_script, insert_values)
+    conn.commit()
+    data = cur.fetchone()
+    return data[0]
+
+# -------------------------------------------------------
+
+
+
+# -------------------------------------------------------
+
+
+
+# -------------------------------------------------------
+
+
+
+# -------------------------------------------------------
+
+
+
+# -------------------------------------------------------
+
+
+
+# -------------------------------------------------------
+
+
+
+# -------------------------------------------------------
+
+
+
+# -------------------------------------------------------
+
